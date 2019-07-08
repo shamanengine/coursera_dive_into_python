ed5f0bf9d91730fac08d776c5a3005dc82ccd0c4
atymchenko <artem.tym@gmail.com>
2019-07-08 20:04:04 +0200

    1

--- converter_sample/test.py
+++ converter_sample/test.py
@@ -2,7 +2,6 @@ import requests
 from decimal import Decimal
 from currency import convert
 
-
 correct = Decimal('3754.8057')
 result = convert(Decimal("1000.1000"), 'RUR', 'JPY', "17/02/2005", requests)
 if result == correct:
