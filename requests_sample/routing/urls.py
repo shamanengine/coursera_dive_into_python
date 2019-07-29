20202519c7f2e9b9271f62076f77a0a63d17ac46
atymchenko <artem.tym@gmail.com>
2019-07-30 00:00:42 +0200

    week 4

--- requests_sample/routing/urls.py
+++ requests_sample/routing/urls.py
@@ -1,5 +1,26 @@
+# from django.conf.urls import url
+# from . import views
+#
+# urlpatterns = [
+#     url(r'^routing/simple_route/$', views.simple_route),
+#     url(r'^routing/slug_route/(?P<slug>[\w\-]{1,16})/$', views.slug_route),
+#     url(r'^routing/sum_route/(?P<n>\-?\d+)/(?P<m>\-?\d+)/$', views.sum_route),
+#     url(r'^routing/sum_get_method/$', views.sum_get_method),
+#     url(r'^routing/sum_post_method/$', views.sum_post_method),
+# ]
+#
+
 from django.conf.urls import url
 
-urlpatterns = [
+from routing.views import (
+    simple_route, slug_route, sum_route,
+    sum_get_method, sum_post_method,
+)
 
+urlpatterns = [
+    url(r'^simple_route/$', simple_route),
+    url(r'^slug_route/([a-z0-9-_]{1,16})/$', slug_route),
+    url(r'^sum_route/(-?\d+)/(-?\d+)/$', sum_route),
+    url(r'^sum_get_method/$', sum_get_method),
+    url(r'^sum_post_method/$', sum_post_method),
 ]
